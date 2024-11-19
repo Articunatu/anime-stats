964aa52bb2e3844f7b128affcff12cd88e4684e3
Articunatu <62301779+Articunatu@users.noreply.github.com>
2024-11-19 13:39:26 +0100

    Paged entry fetch

--- src/services/anime-service.ts
+++ src/services/anime-service.ts
@@ -32,63 +32,73 @@ interface Jpg {
 class AnimeService {
     private readonly _storageKey = 'anime_data';
 
-    public async setPopularAnime(): Promise<Anime[]> {
+   public async setPopularAnime(totalEntries = 100): Promise<Anime[]> {
         const animeListFromStorage = this.loadLocalData();
         if (animeListFromStorage.length > 0) {
             return animeListFromStorage;
         }
-
+    
         const animeList: Anime[] = [];
-        const response = await axios.get<AnimeResponse>(
-            'https://api.jikan.moe/v4/top/anime?type=tv&filter=bypopularity&limit=10&page=2'
-        );
+        const limitPerPage = 10;
+        const maxPages = Math.ceil(totalEntries / limitPerPage);
         
-        if (response.data?.data) {
-            for (const data of response.data.data) {
-                const title = data.title_english || '';
-                const titleSubstring = title.length > 14 ? title.substring(0, 8) : title;
-                const existingAnime = animeListFromStorage.find((anime) =>
-                    anime.title.startsWith(titleSubstring)
-                );
-                if (existingAnime) {
-                    const amountOfSeasons = existingAnime.seasons.length;
-                    const newSeason: Season = {
-                        id: data.malid,
-                        animeId: existingAnime.id,
-                        number: amountOfSeasons + 1,
-                        audienceScore: data.score ?? 0,
-                        amtOfScorers: data.scored_by ?? 0,
-                        amtOfEpisodes: data.episodes,
-                        premier: data.aired?.from ?? '',
-                        finale: data.aired?.to ?? '',
-                        imageUrl: data.images?.jpg.image_url ?? '',
-                    };
-
-                    existingAnime.seasons.push(newSeason);
-                } else {
-                    const newAnime: Anime = {
-                        id: data.malid,
-                        title,
-                        seasons: [
-                            {
-                                id: data.malid,
-                                animeId: Date.now(),
-                                number: 1,
-                                audienceScore: data.score ?? 0,
-                                amtOfScorers: data.scored_by ?? 0,
-                                amtOfEpisodes: 0,
-                                premier: data.aired?.from ?? '',
-                                finale: data.aired?.to ?? '',
-                                imageUrl: data.images?.jpg.image_url ?? ''
-                            },
-                        ],
-                    };
-
-                    animeList.push(newAnime);
+        for (let page = 1; page <= maxPages; page++) {
+            const response = await axios.get<AnimeResponse>(
+                `https://api.jikan.moe/v4/top/anime?type=tv&filter=bypopularity&limit=${limitPerPage}&page=${page}`
+            );
+    
+            if (response.data?.data) {
+                for (const data of response.data.data) {
+                    const title = data.title_english || '';
+                    const titleSubstring = title.length > 14 ? title.substring(0, 8) : title;
+                    const existingAnime = animeListFromStorage.find((anime) =>
+                        anime.title.startsWith(titleSubstring)
+                    );
+                    if (existingAnime) {
+                        const amountOfSeasons = existingAnime.seasons.length;
+                        const newSeason: Season = {
+                            id: data.malid,
+                            animeId: existingAnime.id,
+                            number: amountOfSeasons + 1,
+                            audienceScore: data.score ?? 0,
+                            amtOfScorers: data.scored_by ?? 0,
+                            amtOfEpisodes: data.episodes,
+                            premier: data.aired?.from ?? '',
+                            finale: data.aired?.to ?? '',
+                            imageUrl: data.images?.jpg.image_url ?? '',
+                        };
+    
+                        existingAnime.seasons.push(newSeason);
+                    } else {
+                        const newAnime: Anime = {
+                            id: data.malid,
+                            title,
+                            seasons: [
+                                {
+                                    id: data.malid,
+                                    animeId: Date.now(),
+                                    number: 1,
+                                    audienceScore: data.score ?? 0,
+                                    amtOfScorers: data.scored_by ?? 0,
+                                    amtOfEpisodes: 0,
+                                    premier: data.aired?.from ?? '',
+                                    finale: data.aired?.to ?? '',
+                                    imageUrl: data.images?.jpg.image_url ?? ''
+                                },
+                            ],
+                        };
+    
+                        animeList.push(newAnime);
+                    }
                 }
             }
-            await this.saveLocalData(animeList);
+    
+            if (animeList.length >= totalEntries) {
+                break;
+            }
         }
+    
+        await this.saveLocalData(animeList);
 
         return animeList;
     }
